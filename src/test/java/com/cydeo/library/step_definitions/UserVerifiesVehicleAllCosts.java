73f9ad1cf34cd4d0e94e94643ab2f0dc7dd2d0df
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2022-07-14 12:49:39 -0500

    Updated UserVerifiesVehicleAllCosts

--- src/test/java/com/cydeo/library/step_definitions/UserVerifiesVehicleAllCosts.java
+++ src/test/java/com/cydeo/library/step_definitions/UserVerifiesVehicleAllCosts.java
@@ -7,12 +7,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LoginStepDef {
+public class UserVerifiesVehicleAllCosts {
 
 
     LoginPage loginPage = new LoginPage();
