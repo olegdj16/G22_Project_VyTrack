e360703fd2d9350da867111d1a83cd480ea152e8
olegdj16 <92351568+olegdj16@users.noreply.github.com>
2022-07-14 13:33:08 -0500

    updated

--- src/test/java/com/vytrack/library/pages/LoginPage.java
+++ src/test/java/com/vytrack/library/pages/LoginPage.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 // page_url = https://qa2.vytrack.com/user/login
+
+
+
+// ************* do not make any changes to this file ******************
 public class LoginPage {
 
     @FindBy(xpath = "//*[@id='prependedInput']")
